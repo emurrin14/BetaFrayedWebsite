469ec767e21aa900ebebb9220d931b56a739c545
47332 <47332@students.d94.org>
2026-01-06 19:32:45 +0000

    added shi

--- BetaFrayedApp/urls.py
+++ BetaFrayedApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index, name='index'),
+  path('product/<slug:slug>/', views.Product_View, name='product'),
 ]
--- BetaFrayedApp/views.py
+++ BetaFrayedApp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return render, (request, 'index.html')
+def Index(request):
+    return render(request, 'index.html')
+
+def Product_View(request, slug):
+   product = get_object_or_404(
+        Product.objects.prefetch_related('images', 'variants__size', 'variants__color'), 
+        slug=slug
+   )
+   context = {
+        'product': product,
+   }
+   return render(request, 'product.html', context)
