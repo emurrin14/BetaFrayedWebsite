c29ef80374e5691746a14800a8796e56312353db
47332 <47332@students.d94.org>
2026-01-21 19:13:30 +0000

    responsive mobile cart

--- BetaFrayedApp/urls.py
+++ BetaFrayedApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   path('product/<slug:slug>/', views.Product_View, name='product'),
   path('cart', views.cart_view, name='cart'),
   path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+  path('remove_from_cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
